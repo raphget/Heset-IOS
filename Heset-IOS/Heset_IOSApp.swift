3aee1b9d2323a5faeeffd3501985f06b92ae9091
Raphael Getachew <rva.raph@gmail.com>
2023-03-01 21:42:37 -0500

    initialize ios application

---
+++ Heset-IOS/Heset_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heset_IOSApp.swift
+//  Heset-IOS
+//
+//  Created by Raphael Getachew on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Heset_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
